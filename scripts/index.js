3fe6d07d5bdf3289e6cb92ffe4a2fd8d80d9dad0
ELIORA Smalls <eliorasmalls@gmail.com>
2026-01-16 14:11:39 -0500

    2. Edit Profile form submission

--- scripts/index.js
+++ scripts/index.js
@@ -6,7 +6,7 @@ const editProfileModal = document.querySelector("#edit-profile-modal");
 
 const editProfileCloseBtn = editProfileModal.querySelector(".modal__close-btn");
 
-/*Selecters for editProfileModal above this comment*/
+/*Selecters to open and close editProfileModal above this comment*/
 
 const newPostBtn = document.querySelector(".profile__button-post");
 
@@ -14,7 +14,56 @@ const newPostModal = document.querySelector("#new-post-modal");
 
 const newPostCloseBtn = newPostModal.querySelector(".modal__close-btn");
 
-/* Selecters for newPostModal above this line*/
+/* Selecters to open and close newPostModal above this line*/
+
+const editProfileName = document.querySelector(".profile__name");
+
+const inputEditProfileName = editProfileModal.querySelector(
+  "#profile-name-input"
+);
+
+const editProfileDescription = document.querySelector(".profile__description");
+
+const inputEditProfileDescription = editProfileModal.querySelector(
+  "#profile-description-input"
+);
+/*Selectors to fill in edit modal with current user data above this line*/
+
+/* Selectors to edit profile content once save button is 
+pressed below this line*/
+
+const editProfileFormElement = editProfileModal.querySelector(".modal__form");
+
+const editProfileNameInput = editProfileModal.querySelector(
+  "#profile-name-input"
+);
+
+const editProfileDescriptionInput = editProfileModal.querySelector(
+  "#profile-description-input"
+);
+
+/*IGNORE FOR NOW I MISREAD THE REQUIREMENTS
+const newPostProfileName = document.querySelector(".profile__button-post");
+
+const inputNewPostProfileName = newPostModal.querySelector("#new-post-modal");
+
+const inputNewPostProfileCardImage =
+  newPostModal.querySelector("#card-image-input");
+
+const inputNewPostProfileCardCaption = newPostModal.querySelector(
+  "#card-caption-input"
+);
+
+/*Selectors to fill in new post modal with current user data above this line
+IGNORRRREEE THISSSS */
+
+function fillInputFields() {
+  inputEditProfileName.value = editProfileName.textContent;
+  inputEditProfileDescription.value = editProfileDescription.textContent;
+}
+
+fillInputFields();
+/*Filling in edit profile input fields with current user data above this line*/
 
 editProfileBtn.addEventListener("click", function () {
   editProfileModal.classList.add("modal_is-opened");
@@ -33,3 +82,11 @@ newPostBtn.addEventListener("click", function () {
 newPostCloseBtn.addEventListener("click", function () {
   newPostModal.classList.remove("modal_is-opened");
 });
+
+/*event listeners for new post modal above this line*/
+
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+}
+
+editProfileFormElement.addEventListener("submit", handleProfileFormSubmit);
